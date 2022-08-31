cc1226d33d6c4955febede40031bbda4db7eaf47
“Sabrian <sabrian.salim@gmail.com>
2022-08-31 18:04:41 +1200

    Sabrian blog ready for deployment

---
+++ components/home-page/hero.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}><Image src="/images/site/sabrian.jpg" alt="An image showing myself" width={300} height={300}/></div>
+      <h1>Hi, I'm Sabrian</h1>
+      <p>I blog about web development - especially about React, Node.js and Django.</p>
+    </section>
+  );
+}
+
+export default Hero;
